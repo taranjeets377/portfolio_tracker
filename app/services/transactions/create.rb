eb58b8ef4a585399612860d32fd478fe04a18959
taranjeets377 <taranjeets377@gmail.com>
2026-03-17 15:14:06 +0530

    Add validation to prevent selling more shares than owned in Transactions::Create service

--- app/services/transactions/create.rb
+++ app/services/transactions/create.rb
@@ -7,18 +7,46 @@ module Transactions
 
     def initialize(params)
       @params = params
+      @user = params[:user]
+      @stock = params[:stock]
+      @quantity = params[:quantity]
+      @transaction_type = params[:transaction_type].to_s
     end
 
     def call
-      StockTransaction.create!(
-        user: @params[:user],
-        stock: @params[:stock],
-        platform: @params[:platform],
-        transaction_type: @params[:transaction_type],
-        quantity: @params[:quantity],
-        price: @params[:price],
-        transaction_date: @params[:transaction_date]
-      )
+      ActiveRecord::Base.transaction do
+        validate_sell_quantity! if sell?
+
+        create_transaction!
+      end
+    end
+
+    private
+
+    def sell?
+      @transaction_type == "sell"
+    end
+
+    def validate_sell_quantity!
+      current_quantity = current_stock_quantity
+
+      raise ArgumentError, "Cannot sell more shares than owned" if @quantity > current_quantity
+    end
+
+    def current_stock_quantity
+      buys = @user.stock_transactions
+                  .where(stock: @stock, transaction_type: :buy)
+                  .sum(:quantity)
+
+      sells = @user.stock_transactions
+                   .where(stock: @stock, transaction_type: :sell)
+                   .sum(:quantity)
+
+      buys - sells
+    end
+
+    def create_transaction!
+      StockTransaction.create!(@params)
     end
   end
 end
